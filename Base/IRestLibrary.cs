db45a6a58c2afc91292cf00c922139bab9638a53
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-05-18 18:33:32 +0200

    Add interface and class abstract the properties and methods of the interface

---
+++ Base/IRestLibrary.cs
@@ -0,0 +1,9 @@
+﻿using RestSharp;
+
+namespace GraphQL.NET_API_AutomationTestFramework.Base
+{
+    public interface IRestLibrary
+    {
+        RestClient _restClient { get; }
+    }
+}
---
+++ Base/RestLibrary.cs
@@ -0,0 +1,24 @@
+﻿using RestSharp;
+
+namespace GraphQL.NET_API_AutomationTestFramework.Base
+{
+    public class RestLibrary : IRestLibrary
+    {
+        private readonly RestClientOptions restClientOptions;
+
+        public RestLibrary()
+        {
+            // - Define the Rest Client options, such as base URL, authentication, and other settings
+            this.restClientOptions = new RestClientOptions()
+            {
+                BaseUrl = new Uri("https://localhost:5001/"),
+                RemoteCertificateValidationCallback = (sender, certificate, chain, sslPolicyErrors) => true
+            };
+
+            // - Create a Rest Client instance using the defined options
+            this._restClient = new RestClient(restClientOptions);
+        }
+
+        public RestClient _restClient { get; }
+    }
+}
