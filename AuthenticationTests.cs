5541ecffcbf2588267fb3f7790b9b2d5330b8eda
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-05-18 19:08:54 +0200

    Removed used method

--- AuthenticationTests.cs
+++ AuthenticationTests.cs
@@ -9,70 +9,29 @@ namespace GraphQL.NET_API_AutomationTestFramework
     public class AuthenticationTests
     {
         private readonly ITestOutputHelper testOutputHelper;
-        private RestClient restClient;
+        private readonly IRestFactory _restFactory;
 
-        public AuthenticationTests(ITestOutputHelper testOutputHelper, IRestLibrary restLibrary)
+        public AuthenticationTests(
+            ITestOutputHelper testOutputHelper,
+            IRestFactory restFactory)
         {
             this.testOutputHelper = testOutputHelper;
-            this.restClient = restLibrary._restClient;
+            this._restFactory = restFactory;
         }
         [Fact]
         public async Task PostLogin()
         {
+            var response = await _restFactory.Create()
+                .WithRequest("/api/Authenticate/Login", Method.Post)
+                .WithBody(new
+                {
+                    Username = "kk",
+                    Password = "123456"
+                })
+                .WithPost();
+            var responseContent = response.Content;
+            var token = JObject.Parse(responseContent!)["token"]?.ToString();
             var request = new RestRequest("/api/Authenticate/Login", Method.Post);
-            // Assuming the API expects an anonymous object as JSON body
-            var requestBody = new
-            {
-                Username = "kk",
-                Password = "123456"
-            };
-            request.AddJsonBody(requestBody);
-            var responseObject = restClient.PostAsync(request).Result.Content;
-            var token = JObject.Parse(responseObject!)["token"]?.ToString();
-            //response.IsSuccessful.Should().BeTrue();
-            //var responseContent = response.Content;
-
-            testOutputHelper.WriteLine($"PostLogin Response " +
-                $"\n\tToken: {token}");
-
-            request = new RestRequest("/Product/GetProducts", Method.Get);
-            request.AddHeader("Authorization", $"Bearer {GetToken()}");
-
-            var response = await restClient.GetAsync<List<Product>>(request);
-            testOutputHelper.WriteLine($"Validate: Request Input Is Valid. " +
-                                    $"\n\t Method: {request.Method} " +
-                                    $"\n\t Endpoint: {request.Resource}" +
-                                    $"\n\t Header :  {request.Parameters.FirstOrDefault()!.Value}");
-            testOutputHelper.WriteLine($"Validate: Response Output is Valid. " +
-                                    $"\n\t Total Products: {response!.Count}");
-            foreach (var product in response)
-            {
-                testOutputHelper.WriteLine($"\n\t Product ID: {product.ProductId}" +
-                                    $"\n\t Name: {product.Name}" +
-                                    $"\n\t Price: {product.Price}" +
-                                    $"\n\t Components: {string.Join(", ", 
-                                                        product.Components.Select(c => c.Name))}" +
-                                    $"\n\t Product Type: {product.ProductType}");
-            }
-            response.Count.Should().BeGreaterThan(0);
-            foreach (var product in response)
-            {
-                product.Name.Should().NotBeNullOrEmpty();
-                product.Price.Should().BeGreaterThan(0);
-            }
-        }
-
-        private string GetToken()
-        {
-            var authRequest = new RestRequest("/api/Authenticate/Login", Method.Post);
-            var requestBody = new {
-                UserName = "kk",
-                Password = "123456"
-            };
-            
-            var authResponse = restClient.PostAsync(authRequest.AddJsonBody(requestBody)).Result.Content;
-
-            return JObject.Parse(authResponse!)["token"]?.ToString() ?? string.Empty;
         }
     }
 }
