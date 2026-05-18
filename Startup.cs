4d2eebcf9670b1f53ac60aef2c17479822edb1df
Sivatho Conjwa <SConjwa@clientele.co.za>
2026-05-18 18:37:10 +0200

    Created class for dependecy injection

---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using GraphQL.NET_API_AutomationTestFramework.Base;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL.NET_API_AutomationTestFramework
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services
+                .AddSingleton<IRestLibrary, RestLibrary>()
+                .AddScoped<IRestBuilder, RestBuilder>()
+                .AddScoped<IRestFactory, RestFactory>();
+        }
+    }
+}
